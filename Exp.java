9899a8a8015c584803b19f8b7b7708cedf8a8230
panindrap67 <135000786+panindrap67@users.noreply.github.com>
2023-05-30 14:23:17 +0530

    Add files via upload

---
+++ Exp.java
@@ -0,0 +1,16 @@
+class Exp
+{
+	public static void main(String s[])
+	{
+	int i=1;
+	int sum=0;
+	while(i<=10)
+	{
+	sum=sum+i;
+	i++;
+	System.out.println("sum="+sum);
+	System.out.println("i="+i);
+	}
+	System.out.println("sum of 1to 10"+"is"+sum);
+	}
+}
